884264bf81db320e0bee40a334bf43360a4f376a
Patrick Knauf <39289173+Patrickk17@users.noreply.github.com>
2020-10-03 17:42:39 -0500

    Update save.sh

--- scripts/save.sh
+++ scripts/save.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+exec mkdir /server-saves/oct-3-world-save/
 exec cp -R world world_nether world_the_end Epic_Minecrafters Epic_Minecrafters_nether Epic_Minecrafters_the_end NewSurvival NewSurvival_nether NewSurvival_the_end /server-saves/oct-3-world-save/
