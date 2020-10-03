b33c88d8c44a36cfbcfff65778d2cff394786dba
Patrick Knauf <39289173+Patrickk17@users.noreply.github.com>
2020-10-03 17:41:52 -0500

    Create save.sh

---
+++ scripts/save.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec cp -R world world_nether world_the_end Epic_Minecrafters Epic_Minecrafters_nether Epic_Minecrafters_the_end NewSurvival NewSurvival_nether NewSurvival_the_end /server-saves/oct-3-world-save/
